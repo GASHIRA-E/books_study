54dea9a214934c98611e68e32219db74d69f6194
GASHIRA-E <dsteams@outlook.jp>
2020-04-19 16:09:38 +0900

    制限付きポリモーフィズムを少し書いた。

--- oreilly_typescript/src/section3.ts
+++ oreilly_typescript/src/section3.ts
@@ -6,3 +6,14 @@ enum A {
 let a = A[3];
 
 console.log(a); // undefined
+
+type Cat = {name: string, purrs: boolean};
+type Dog = {name: string, barks: boolean, wags: boolean};
+
+type CatOrDogOtBoth = Cat | Dog;
+
+const testAnimal: CatOrDogOtBoth = {
+  name: 'tama',
+  barks: false,
+  wags: false
+}
